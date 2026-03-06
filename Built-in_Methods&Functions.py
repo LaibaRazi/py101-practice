7388f543e117b75fc2f85f5aea40ec32ff3a8e1b
LaibaRazi <khanlayba72@gmail.com>
2026-03-06 09:17:53 +0500

    set's n methods comment

---
+++ Built-in_Methods&Functions.py
@@ -0,0 +1,31 @@
+a ={"a@gmail.com","b@gmail.com","a@gmail.com"}
+if len(a) == len(set(a)):
+    print(a)
+    print(set(a))
+    print(len(a))
+    print(len(set(a)))
+    print("there is a duplicate")
+    '''Set = {unique elements}
+
+Properties
+---------
+No duplicates
+Unordered
+No indexing
+Mutable
+Fast lookup
+
+Operations
+----------
+add()
+union()
+intersection()
+difference()
+clear()
+
+Operators
+---------
+| union
+& intersection
+- difference
+^ symmetric difference'''
